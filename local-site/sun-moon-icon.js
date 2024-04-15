93d96034e61b20db78c0aa0565f20b1f05b365ed
kipkulei <kipkulei@princeton.edu>
2024-04-15 18:36:33 -0400

    sun-moon 2

--- local-site/sun-moon-icon.js
+++ local-site/sun-moon-icon.js
@@ -2,10 +2,10 @@ const currentHour = new Date().getHours();
 
 const iconElement = document.getElementById("sun-moon-icon");
 
-if (currentHour >= 6 && currentHour < 18) {
+if (currentHour >= 6 && currentHour < 20) {
     // It's daytime, so add the sun icon
-    iconElement.innerHTML = '<img src="local_site_images/sun.jpg" alt="Sun Icon">';
+    iconElement.innerHTML = '<img src="local_site_images/sun.jpeg" alt="Sun Icon">';
 } else {
     // It's nighttime, so add the moon icon
-    iconElement.innerHTML = '<img src="local_site_images/moon.jpg" alt="Moon Icon">';
+    iconElement.innerHTML = '<img src="local_site_images/moon.jpeg" alt="Moon Icon">';
 }
